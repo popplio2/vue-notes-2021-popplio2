9ea17fe95f02f27a6b8424bddea97c029faa1944
popplio2 <danikbrisk@yahoo.com>
2021-06-05 21:20:35 -0400

    lessons 7 and 8 done

--- app.js
+++ app.js
@@ -1,23 +1,15 @@
 new Vue({
     el: '#vue-app',
     data: {
-        age: 25,
-        x: 0,
-        y: 0
+        name: "",
+        age: "",
     },
     methods: {
-        add: function(inc){
-            this.age += inc;
+        logName: function () {
+            console.log("You entered your name, fool.");
         },
-        subtract: function(dec){
-            this.age -= dec;
-        },
-        updateXY: function(event){
-            this.x = event.offsetX;
-            this.y = event.offsetY;
-        },
-        click: function(){
-            alert('Daniel is awesome.');
+        logAge: function () {
+            console.log("You entered your age, fool.");
         }
     }
 });
