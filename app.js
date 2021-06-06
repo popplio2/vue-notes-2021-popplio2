8175d4a3bb52523fdfccebe6f8ad7835d909b0b8
popplio2 <danikbrisk@yahoo.com>
2021-06-05 20:18:25 -0400

    lessons 5 and 6 done

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
-new Vue ({
+new Vue({
     el: '#vue-app',
     data: {
-        name: 'Daniel',
-        job: 'Pokemon Master',
-        website: 'http://pokemoncenter.com',
-        websiteTag: '<a href="http://pokemoncenter.com">Get awesome Pokemon clothes</a>'
+        age: 25,
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function (time) {
-            return "Good " + time + ", " + this.name;
+        add: function(inc){
+            this.age += inc;
+        },
+        subtract: function(dec){
+            this.age -= dec;
+        },
+        updateXY: function(event){
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        },
+        click: function(){
+            alert('Daniel is awesome.');
         }
     }
 });
