c21bc2bec237186b5b0d0171965d120b55e3aa5c
popplio2 <danikbrisk@yahoo.com>
2021-06-08 22:58:59 -0400

    Finished pokemon version of punchbag game!!

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 new Vue({
     el: '#vue-app',
     data: {
-        name: 'Daniel',
-        characters: ['Pikachu', 'Bulbasaur', 'Squirtle', 'Charmander'],
-        trainers: [
-            {name: 'Ash', age: 10},
-            {name: 'Brock', age: 16},
-            {name: 'Misty', age: 13},
-        ],
+        health: 100,
+        ended: false,
     },
     methods: {
+        fight: function () {
+            this.health -= 10;
+            if (this.health <= 0) {
+                this.ended = true;
+                alert("Pikachu fainted!");
+            }
+            console.log(this.health);
+        },
+        restart: function () {
+            this.health = 100;
+            this.ended = false;
+            console.log(this.health);
+        },
     },
     computed: {
        
     },
 });  
+
