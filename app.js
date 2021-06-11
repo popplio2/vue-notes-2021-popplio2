0450dc14144ef0190afd5ab925b7a53ea64b761e
popplio2 <danikbrisk@yahoo.com>
2021-06-11 01:55:35 -0400

    Finished lesson 16

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-Vue.component('greeting', {
-    template: '<p>IM A {{ name }}. <button v-on:click="changeName">Change name</button></p>',
-    data: function () {
-        return {
-            name: 'Yoshi',
-        }
+new Vue ({
+    el: '#vue-app',
+    data: {
+        output: 'Your fav food'
     },
     methods: {
         changeName: function () {
             this.name = 'Mario';
         }
     }
-});
-
-new Vue ({
-    el: '#vue-app-one',
-});
-
-new Vue ({
-    el: '#vue-app-two',
 });
