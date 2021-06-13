3bb0abb8b6d4a041367a6102917f78c75489be8b
popplio2 <danikbrisk@yahoo.com>
2021-06-13 12:43:06 -0400

    vue cli works babeyyyy

---
+++ my-project/src/Ninjas.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul id="app">
+    <li v-for="ninja in ninjas">{{ ninja }}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+     ninjas: ['Daniel', 'Charley', 'Alston']
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
