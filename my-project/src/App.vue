a7ca0d0debd3e7370d09b961019906b7a711bde5
popplio2 <danikbrisk@yahoo.com>
2021-06-18 16:20:16 -0400

    Nothing is working

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
+  <div>
+    <app-header></app-header>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
-import Ninjas from './Ninjas.vue'
+import Header from "./components/Header.vue"
+import Footer from "./components/Footer.vue"
 
 export default {
   components: {
-    'ninjas': Ninjas
+    'app-header': Header,
+    'app-footer': Footer
   },
   data() {
     return {
-      title: 'Ninja App'
+
     }
   }
 }
 </script>
 
-<style scoped>
-  h1 {
-    color: purple;
-  }
+<style>
+
 </style>
